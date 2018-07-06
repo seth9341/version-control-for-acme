65599fcd10dca7d7eabd532be2469c456a7a80dd
seth9341 <seth9341@gmail.com>
2018-07-05 19:44:36 -0500

    enhancement 9

---
+++ model/uploads-model.php
@@ -0,0 +1,61 @@
+<?php
+// Add image information to database
+function storeImages($imgPath, $prodId, $imgName) {
+    $db = acmeConnect();
+    $sql = 'INSERT INTO images (invId, imgPath, imgName) VALUES (:prodId, :imgPath, :imgName)';
+    $stmt = $db->prepare($sql);
+    // Store full size image information
+    $stmt->bindValue(':prodId', $prodId, PDO::PARAM_INT);
+    $stmt->bindValue(':imgPath', $imgPath, PDO::PARAM_STR);
+    $stmt->bindValue(':imgName', $imgName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    // Make and store the thumbnail image information
+    // Change name in path
+    $imgPath = makeThumbnailName($imgPath);
+    // Change name in file name
+    $imgName = makeThumbnailName($imgName);
+    $stmt->bindValue(':prodId', $prodId, PDO::PARAM_INT);
+    $stmt->bindValue(':imgPath', $imgPath, PDO::PARAM_STR);
+    $stmt->bindValue(':imgName', $imgName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $rowsChanged = $stmt->rowCount();
+    $stmt->closeCursor();
+    return $rowsChanged;
+}
+
+// Get Image Information from images table
+function getImages() {
+    $db = acmeConnect();
+    $sql = 'SELECT imgId, imgPath, imgName, imgDate, inventory.invId, invName FROM images JOIN inventory ON images.invId = inventory.invId';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $imageArray = $stmt->fetchAll(PDO::FETCH_NAMED);
+    $stmt->closeCursor();
+    return $imageArray;
+}
+
+// Delete image information from images table
+function deleteImage($id) {
+    $db = acmeConnect();
+    $sql = 'DELETE FROM images WHERE imgId = :imgId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':imgId', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->rowCount();
+    $stmt->closeCursor();
+    return $result;
+}
+
+// Check for existing image
+function checkExistingImage($name) {
+    $db = acmeConnect();
+    $sql = "SELECT imgName FROM images WHERE imgName = :name";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->execute();
+    $imageMatch = $stmt->fetch();
+    $stmt->closeCursor();
+    return $imageMatch;
+}
